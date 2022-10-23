b4b65afba9bc8e78b1cab789cf26d64834e65ff0
karlodad <poka-riteli@mail.ru>
2022-10-23 11:33:43 +0300

    #4 Добавлены страницы авторизации/регистрации #6 добавлена базовая логика защиты от неавторизированных пользователей

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -4,6 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import AuthComponent from './auth.component';
 import LoginComponent from './login/login.component';
 import RegistrationComponent from './registration/registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
 
 const routes: Routes = [
   {
@@ -28,6 +35,17 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, RegistrationComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzButtonModule,
+    NzIconModule,
+    NzFormModule,
+    NzSpinModule,
+    NzAlertModule,
+  ],
 })
 export default class AuthModule {}
---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,125 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { firstValueFrom, interval, Subscriber, Subscription } from 'rxjs';
+import { User } from '../auth/models/user.model';
+
+export type ReturnUserTokens = { refreshToken: string; accessToken: string; datalife: string };
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  public user: User | null = null;
+  private tokens: ReturnUserTokens | null = null;
+  private sub: Subscription | null = null;
+  private readonly LOCAL_STORAGE_NAME = 'auth-settings';
+
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  async login(user: User): Promise<void> {
+    const tokens = this.getTokens();
+    if (tokens) {
+      throw new Error('Пользователь уже авторизован!');
+    }
+    const answer = await firstValueFrom(
+      this.httpClient.post<ReturnUserTokens>('http://localhost:4000/user/login', user),
+    );
+    this.saveTokens(answer);
+    this.user = user;
+    this.startWatchRefreshToken();
+  }
+
+  async registration(user: User): Promise<void> {
+    const tokens = this.getTokens();
+    if (tokens) {
+      throw new Error('Пользователь уже авторизован!');
+    }
+    const answer = await firstValueFrom(
+      this.httpClient.post<ReturnUserTokens>('http://localhost:4000/user/signup', user),
+    );
+    this.saveTokens(answer);
+    this.user = user;
+    this.startWatchRefreshToken();
+  }
+
+  async logout() {
+    const tokens = this.getTokens();
+    if (!tokens) {
+      throw new Error('Пользователь не авторизован!');
+    }
+    try {
+      await firstValueFrom(this.httpClient.post<void>('http://localhost:4000/user/logout', {}));
+    } catch {}
+    this.clearToken();
+    this.stopWatchRefreshToken();
+    this.router.navigate(['/auth']);
+  }
+
+  public startWatchRefreshToken() {
+    console.log('start watch auth');
+    const tokens = this.getTokens();
+    if (!tokens) {
+      return this.stopWatchRefreshToken();
+    }
+    if (this.sub) {
+      this.stopWatchRefreshToken();
+    }
+    const future = new Date(tokens.datalife).getTime();
+    const current = new Date().getTime();
+    const fullMinute = (future - current) / 1000 / 60;
+    const refreshMinute = (fullMinute * 80) / 100;
+    this.sub = interval(Math.round(refreshMinute * 60 * 1000)).subscribe((e) => this.refreshToken());
+  }
+
+  public stopWatchRefreshToken() {
+    console.log('stop watch token');
+    this.sub?.unsubscribe();
+    this.sub = null;
+  }
+
+  public async refreshToken() {
+    console.log('refresh token');
+    const tokens = this.getTokens();
+    if (!tokens) {
+      throw new Error('Пользователь не авторизован!');
+    }
+    try {
+      this.stopWatchRefreshToken();
+      const answer = await firstValueFrom(
+        this.httpClient.post<ReturnUserTokens>('http://localhost:4000/user/refresh', {
+          refreshToken: tokens.refreshToken,
+        }),
+      );
+      this.saveTokens(answer);
+      this.startWatchRefreshToken();
+      console.warn('token update success', answer.datalife);
+    } catch {
+      console.warn('token refresh failed');
+      this.stopWatchRefreshToken();
+      this.logout();
+    }
+  }
+
+  public getTokens(): ReturnUserTokens | null {
+    try {
+      if (this.tokens) {
+        return this.tokens;
+      }
+      const token = JSON.parse(localStorage.getItem(this.LOCAL_STORAGE_NAME) ?? '');
+      return (this.tokens = token);
+    } catch {
+      return null;
+    }
+  }
+
+  private saveTokens(token: ReturnUserTokens): ReturnUserTokens {
+    localStorage.setItem(this.LOCAL_STORAGE_NAME, JSON.stringify(token));
+    return (this.tokens = token);
+  }
+
+  private clearToken() {
+    localStorage.removeItem(this.LOCAL_STORAGE_NAME);
+    this.tokens = null;
+  }
+}
